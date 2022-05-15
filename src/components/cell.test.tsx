e2132f35063d5c3079ec9146c089c83a73b7f19e
RomanNevmirich <97444554+RomanNevmirich@users.noreply.github.com>
2022-05-15 14:00:46 +0500

    fix: исправлен render тест на Cell

--- src/components/cell.test.tsx
+++ src/components/cell.test.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import { render, screen, fireEvent, createEvent } from '@testing-library/react';
-import renderer from 'react-test-renderer';
-
-
 import { Cell } from './cell';
 
 describe('Cell component check', () => {
-  
-  it('renders correctly', () => {
-    const tree = renderer
-      .create(<Cell value={1}></Cell>)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
   
   it('Cell render fill', () => {
     const { asFragment } = render(<Cell value={1}></Cell>);
